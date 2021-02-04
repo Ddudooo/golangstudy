b27ac1f41e8f85225d0930a2df8b768367f539d3
LeeJungdoo <jd.lee@softwareinlife.com>
2021-02-04 15:13:42 +0900

    first commit

---
+++ hello/hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+	//message, err := greetings.Hello("Grady")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
+}
